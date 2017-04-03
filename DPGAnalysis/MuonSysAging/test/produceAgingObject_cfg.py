98b06ea064821830d6f771395bf03b2dd9c053a3
Carlo Battilana <carlo.battilana@gmail.com>
2017-04-03 22:50:11 +0200

    Cfg RegEx compliant

--- DPGAnalysis/MuonSysAging/test/produceAgingObject_cfg.py
+++ DPGAnalysis/MuonSysAging/test/produceAgingObject_cfg.py
@@ -96,32 +96,32 @@ process.produceAgingObject = cms.EDAnalyzer("ProduceAgingObject",
             # A chamber by chamber list in format CHAMBER:EFF
 
             # MB4 of top sectors with EFF = 0
-            "WH-2_ST4_SEC2:0.","WH-2_ST4_SEC3:0.","WH-2_ST4_SEC4:0.",
-            "WH-2_ST4_SEC5:0.","WH-2_ST4_SEC6:0.","WH-1_ST4_SEC2:0.",
-            "WH-1_ST4_SEC3:0.","WH-1_ST4_SEC4:0.","WH-1_ST4_SEC5:0.",
-            "WH-1_ST4_SEC6:0.","WH0_ST4_SEC2:0.","WH0_ST4_SEC3:0.",
-            "WH0_ST4_SEC4:0.","WH0_ST4_SEC5:0.","WH0_ST4_SEC6:0.",
-            "WH1_ST4_SEC2:0.","WH1_ST4_SEC3:0.","WH1_ST4_SEC4:0.",
-            "WH1_ST4_SEC5:0.","WH1_ST4_SEC6:0.","WH2_ST4_SEC2:0.",
-            "WH2_ST4_SEC3:0.","WH2_ST4_SEC4:0.","WH2_ST4_SEC5:0.",
-            "WH2_ST4_SEC6:0.",
+            "WH-2_ST4_SEC2$:0.","WH-2_ST4_SEC3$:0.","WH-2_ST4_SEC4$:0.",
+            "WH-2_ST4_SEC5$:0.","WH-2_ST4_SEC6$:0.","WH-1_ST4_SEC2$:0.",
+            "WH-1_ST4_SEC3$:0.","WH-1_ST4_SEC4$:0.","WH-1_ST4_SEC5$:0.",
+            "WH-1_ST4_SEC6$:0.","WH0_ST4_SEC2$:0.","WH0_ST4_SEC3$:0.",
+            "WH0_ST4_SEC4$:0.","WH0_ST4_SEC5$:0.","WH0_ST4_SEC6$:0.",
+            "WH1_ST4_SEC2$:0.","WH1_ST4_SEC3$:0.","WH1_ST4_SEC4$:0.",
+            "WH1_ST4_SEC5$:0.","WH1_ST4_SEC6$:0.","WH2_ST4_SEC2$:0.",
+            "WH2_ST4_SEC3$:0.","WH2_ST4_SEC4$:0.","WH2_ST4_SEC5$:0.",
+            "WH2_ST4_SEC6$:0.",
             # MB1 of external wheels with EFF = 0
-            "WH-2_ST1_SEC1:0.","WH-2_ST1_SEC2:0.","WH-2_ST1_SEC3:0.",
-            "WH-2_ST1_SEC4:0.","WH-2_ST1_SEC5:0.","WH-2_ST1_SEC6:0.",
-            "WH-2_ST1_SEC7:0.","WH-2_ST1_SEC8:0.","WH-2_ST1_SEC9:0.",
-            "WH-2_ST1_SEC10:0.","WH-2_ST1_SEC11:0.","WH-2_ST1_SEC12:0.",
-            "WH2_ST1_SEC1:0.","WH2_ST1_SEC2:0.","WH2_ST1_SEC3:0.",
-            "WH2_ST1_SEC4:0.","WH2_ST1_SEC5:0.","WH2_ST1_SEC6:0.",
-            "WH2_ST1_SEC7:0.","WH2_ST1_SEC8:0.","WH2_ST1_SEC9:0.",
-            "WH2_ST1_SEC10:0.","WH2_ST1_SEC11:0.","WH2_ST1_SEC12:0.",
+            "WH-2_ST1_SEC1$:0.","WH-2_ST1_SEC2$:0.","WH-2_ST1_SEC3$:0.",
+            "WH-2_ST1_SEC4$:0.","WH-2_ST1_SEC5$:0.","WH-2_ST1_SEC6$:0.",
+            "WH-2_ST1_SEC7$:0.","WH-2_ST1_SEC8$:0.","WH-2_ST1_SEC9$:0.",
+            "WH-2_ST1_SEC10$:0.","WH-2_ST1_SEC11$:0.","WH-2_ST1_SEC12$:0.",
+            "WH2_ST1_SEC1$:0.","WH2_ST1_SEC2$:0.","WH2_ST1_SEC3$:0.",
+            "WH2_ST1_SEC4$:0.","WH2_ST1_SEC5$:0.","WH2_ST1_SEC6$:0.",
+            "WH2_ST1_SEC7$:0.","WH2_ST1_SEC8$:0.","WH2_ST1_SEC9$:0.",
+            "WH2_ST1_SEC10$:0.","WH2_ST1_SEC11$:0.","WH2_ST1_SEC12$:0.",
             # 5 MB2s of external wheels with EFF = 0
-            "WH2_ST2_SEC3:0.","WH2_ST2_SEC6:0.","WH2_ST2_SEC9:0.",
-            "WH-2_ST2_SEC2:0.","WH-2_ST2_SEC4:0.",
+            "WH2_ST2_SEC3$:0.","WH2_ST2_SEC6$:0.","WH2_ST2_SEC9$:0.",
+            "WH-2_ST2_SEC2$:0.","WH-2_ST2_SEC4$:0.",
             # more sparse failures with EFF = 0
-            "WH-2_ST2_SEC8:0.","WH-1_ST1_SEC1:0.","WH-1_ST2_SEC1:0.",
-            "WH-1_ST1_SEC4:0.","WH-1_ST3_SEC7:0.","WH0_ST2_SEC2:0.",
-            "WH0_ST3_SEC5:0.","WH0_ST4_SEC12:0.","WH1_ST1_SEC6:0.",
-            "WH1_ST1_SEC10:0.","WH1_ST3_SEC3:0."
+            "WH-2_ST2_SEC8$:0.","WH-1_ST1_SEC1$:0.","WH-1_ST2_SEC1$:0.",
+            "WH-1_ST1_SEC4$:0.","WH-1_ST3_SEC7$:0.","WH0_ST2_SEC2$:0.",
+            "WH0_ST3_SEC5$:0.","WH0_ST4_SEC12$:0.","WH1_ST1_SEC6$:0.",
+            "WH1_ST1_SEC10$:0.","WH1_ST3_SEC3$:0."
 
             # Or a RegEx setting efficiency  for all chamber to 10%
             #"(WH-?\\\d_ST\\\d_SEC\\\\d+):0.1"
